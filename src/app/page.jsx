cc585f85a3bc55bca800ee1ae38d5c0c9f38db91
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-11-20 23:32:19 +0100

    first go at the main page navbar

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import icon from './images/whatbytes_icon.png'
+import profile_pic from './images/profile_picture.jpg'
+
+const page = () => {
+  return (
+    <main className='background min-h-screen flex flex-col text-sm sm:text-base'>
+      <nav>
+        <div>
+          <img src={icon} alt='whatbytes icon'/>
+          <h1>WhatBytes</h1>
+        </div>
+        <div>
+          <img src={profile_pic} alt='profile picture'/>
+          <p>Rahil Siddique</p>
+        </div>
+      </nav>
+    </main>
+  )
+}
+
+export default page
